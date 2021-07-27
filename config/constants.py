dcc2907757ee384157ae3efb0f17798e1cf5e78e
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-07-27 16:51:40 +0300

    update gitignore

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+# Глобальные константы (настройки)
+WIN_X = 800
+WIN_Y = 600
+
+FPS = 60
+
+GRAVITY = 0.15
+HERO_JUMP = -7
+HERO_SPEED = 5
+HERO_START_LIVES = 3
+
+
+C_WHITE = (255, 255, 255)
+C_DARK = (48, 48, 0)
+C_YELLOW = (255, 255, 87)
+C_GREEN = (32, 128, 32)
